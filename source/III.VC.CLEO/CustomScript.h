ca76ef8482a1692afdb934906954893b58755267
firedihm <42544757+firedihm@users.noreply.github.com>
2025-09-03 22:19:48 +0300

    Update CustomScript.h

--- source/III.VC.CLEO/CustomScript.h
+++ source/III.VC.CLEO/CustomScript.h
@@ -6,13 +6,13 @@
 
 #include "ScmFunction.h"
 
-enum eScriptType :unsigned short
+enum eScriptType : unsigned short
 {
 	SCRIPT_TYPE_DEFAULT = 0,
 	SCRIPT_TYPE_CUSTOM = 1
 };
 
-enum eOpcodeResult :char
+enum eOpcodeResult : char
 {
 	OR_CONTINUE = 0,
 	OR_TERMINATE = 1,
@@ -25,12 +25,12 @@ union tScriptVar
 {
 	int nVar;
 	float fVar;
-	char *cVar;
-	void *pVar;
+	char* cVar;
+	void* pVar;
 	DWORD dVar;
 };
 
-enum eParamType :unsigned char
+enum eParamType : unsigned char
 {
 	PARAM_TYPE_END_OF_PARAMS = 0,
 	PARAM_TYPE_INT32 = 1,
@@ -50,55 +50,47 @@ struct tParamType
 
 class CScript
 {
-public:
-	/* 0x00 */ CScript *m_pNext;
-	/* 0x04 */ CScript *m_pPrev;
-	/* 0x08 */ char m_acName[8];
-	/* 0x10 */ unsigned int m_dwIp;
-	/* 0x14 */ unsigned int m_aGosubAddr[6];
-	/* 0x2C */ unsigned short m_nCurrentGosub;
-	/* 0x2E */ unsigned short m_nScriptType; // see eScriptType
-	/* 0x30 */ tScriptVar m_aLVars[18];
-#if CLEO_VC
-	/* 0x78 */ bool m_bIsActive; 
-	/* 0x79 */ bool m_bCondResult; 
-	/* 0x7A */ bool m_bIsMission;
-#else
-	/* 0x78 */ bool m_bCondResult;
-	/* 0x79 */ bool m_bIsMission;
-	/* 0x7A */ bool m_bIsActive;
-#endif
-	/* 0x7B */ bool m_bAwake;
-	/* 0x7C */ unsigned int m_dwWakeTime;
-	/* 0x80 */ unsigned short m_wIfOp;
-	/* 0x82 */ bool m_bNotFlag;
-	/* 0x83 */ bool m_bDeathArrestCheckEnabled;
-	/* 0x84 */ bool m_bWastedOrBusted;
-	/* 0x85 */ bool m_bMissionFlag;
-	/* 0x86 */ short _pad;
+	public:
+		/* 0x00 */ CScript* m_pNext;
+		/* 0x04 */ CScript* m_pPrev;
+		/* 0x08 */ char m_acName[8];
+		/* 0x10 */ unsigned int m_dwIp;
+		/* 0x14 */ unsigned int m_aGosubAddr[6];
+		/* 0x2C */ unsigned short m_nCurrentGosub;
+		/* 0x2E */ eScriptType m_nScriptType;
+		/* 0x30 */ tScriptVar m_aLVars[18];
+	#if CLEO_VC
+		/* 0x78 */ bool m_bIsActive; 
+		/* 0x79 */ bool m_bCondResult; 
+		/* 0x7A */ bool m_bIsMission;
+	#else
+		/* 0x78 */ bool m_bCondResult;
+		/* 0x79 */ bool m_bIsMission;
+		/* 0x7A */ bool m_bIsActive;
+	#endif
+		/* 0x7B */ bool m_bAwake;
+		/* 0x7C */ unsigned int m_dwWakeTime;
+		/* 0x80 */ unsigned short m_wIfOp;
+		/* 0x82 */ bool m_bNotFlag;
+		/* 0x83 */ bool m_bDeathArrestCheckEnabled;
+		/* 0x84 */ bool m_bWastedOrBusted;
+		/* 0x85 */ bool m_bMissionFlag;
+		/* 0x86 */ short _pad;
 
-	/* Custom data */
-	/* 0x88 */ unsigned int m_nLastPedSearchIndex;
-	/* 0x8C */ unsigned int m_nLastVehicleSearchIndex;
-	/* 0x90 */ unsigned int m_nLastObjectSearchIndex;
-	/* 0x94 */ struct _LoadingErrors{
-				   unsigned int m_bFileNotFound : 1;
-				   unsigned int m_bFileSeekError : 1;
-				   unsigned int m_bInternalError : 1;
-				   unsigned int m_bEmptyFile : 1;
-				   unsigned int m_bAllocationFailed : 1;
-				   unsigned int m_bEofReached : 1;
-			   } m_Errors;
-	/* 0x98 */ char *m_pCodeData;
-	/* 0x9C */ unsigned int m_dwBaseIp;
-	/* 0xA0 */ class ScmFunction *m_pScmFunction;
-	/* 0xA4 */ CScript *m_pNextCustom;
-	/* 0xA8 */ CScript *m_pPrevCustom;
-	/* 0xAC */ tScriptVar *m_pLocalArray;
+		/* Custom data */
+		/* 0x88 */ unsigned int m_nLastPedSearchIndex;
+		/* 0x8C */ unsigned int m_nLastVehicleSearchIndex;
+		/* 0x90 */ unsigned int m_nLastObjectSearchIndex;
+		/* 0x94 */ unsigned int _padd;
+		/* 0x98 */ char* m_pCodeData;
+		/* 0x9C */ unsigned int m_dwBaseIp;
+		/* 0xA0 */ class ScmFunction* m_pScmFunction;
+		/* 0xA4 */ CScript* m_pNextCustom;
+		/* 0xA8 */ CScript* m_pPrevCustom;
+		/* 0xAC */ tScriptVar* m_pLocalArray;
 
-	void Init();
-
-	bool Loaded();
+		CScript(char* filepath);
+		~CScript();
 
 	void AddToCustomList(CScript **list);
 
@@ -120,15 +112,9 @@ public:
 
 	CLEOAPI void *GetPointerToScriptVariable();
 
-	CScript() = default;
-
-	CScript(char *filepath);
-
-	~CScript();
-
 	CLEOAPI void JumpTo(int address);
 
 	eOpcodeResult ProcessOneCommand();
 };
 
-static_assert(sizeof(CScript) == 0xB0, "Error with CScript");
+static_assert(sizeof(CScript) == 0xB0, "CScript size mismatch");
