be5fc3f35fbfb856ef6c0cf56cf13c35ccca7959
firedihm <42544757+firedihm@users.noreply.github.com>
2025-09-04 20:41:08 +0300

    Update CustomScript.h

--- source/III.VC.CLEO/CustomScript.h
+++ source/III.VC.CLEO/CustomScript.h
@@ -6,11 +6,7 @@
 
 #include "ScmFunction.h"
 
-enum eScriptType : unsigned short
-{
-	SCRIPT_TYPE_DEFAULT = 0,
-	SCRIPT_TYPE_CUSTOM = 1
-};
+#define KEY_LENGTH_IN_SCRIPT (8)
 
 enum eOpcodeResult : char
 {
@@ -53,11 +49,12 @@ class CScript
 	public:
 		/* 0x00 */ CScript* m_pNext;
 		/* 0x04 */ CScript* m_pPrev;
-		/* 0x08 */ char m_acName[8];
-		/* 0x10 */ unsigned int m_dwIp;
-		/* 0x14 */ unsigned int m_aGosubAddr[6];
-		/* 0x2C */ unsigned short m_nCurrentGosub;
-		/* 0x2E */ eScriptType m_nScriptType;
+		/* 0x08 */ char m_acName[KEY_LENGTH_IN_SCRIPT];
+		/* 0x10 */ uint m_dwIp;
+		/* 0x14 */ uint m_aGosubAddr[6];
+		/* 0x2C */ ushort m_nCurrentGosub;
+		/* 0x2E */ bool m_bIsCustom;
+		/* 0x2F */ bool m_bIsPersistent;
 		/* 0x30 */ tScriptVar m_aLVars[18];
 	#if CLEO_VC
 		/* 0x78 */ bool m_bIsActive; 
@@ -69,8 +66,8 @@ class CScript
 		/* 0x7A */ bool m_bIsActive;
 	#endif
 		/* 0x7B */ bool m_bAwake;
-		/* 0x7C */ unsigned int m_dwWakeTime;
-		/* 0x80 */ unsigned short m_wIfOp;
+		/* 0x7C */ uint m_dwWakeTime;
+		/* 0x80 */ ushort m_wIfOp;
 		/* 0x82 */ bool m_bNotFlag;
 		/* 0x83 */ bool m_bDeathArrestCheckEnabled;
 		/* 0x84 */ bool m_bWastedOrBusted;
@@ -78,13 +75,13 @@ class CScript
 		/* 0x86 */ short _pad;
 
 		/* Custom data */
-		/* 0x88 */ unsigned int m_nLastPedSearchIndex;
-		/* 0x8C */ unsigned int m_nLastVehicleSearchIndex;
-		/* 0x90 */ unsigned int m_nLastObjectSearchIndex;
-		/* 0x94 */ unsigned int _padd;
+		/* 0x88 */ uint m_nLastPedSearchIndex;
+		/* 0x8C */ uint m_nLastVehicleSearchIndex;
+		/* 0x90 */ uint m_nLastObjectSearchIndex;
+		/* 0x94 */ uint _padd;
 		/* 0x98 */ char* m_pCodeData;
-		/* 0x9C */ unsigned int m_dwBaseIp;
-		/* 0xA0 */ class ScmFunction* m_pScmFunction;
+		/* 0x9C */ uint m_dwBaseIp;
+		/* 0xA0 */ ScmFunction* m_pScmFunction;
 		/* 0xA4 */ CScript* m_pNextCustom;
 		/* 0xA8 */ CScript* m_pPrevCustom;
 		/* 0xAC */ tScriptVar* m_pLocalArray;
@@ -92,29 +89,30 @@ class CScript
 		CScript(char* filepath);
 		~CScript();
 
-	void AddToCustomList(CScript **list);
+		void AddToCustomList(CScript** list);
+		void RemoveFromCustomList(CScript** list);
 
-	void RemoveFromCustomList(CScript **list);
+	// exports
+	CLEOAPI void Collect(uint numParams);
 
-	CLEOAPI void Collect(unsigned int numParams);
+	CLEOAPI void Collect(uint* pIp, unsigned int numParams);
 
-	CLEOAPI void Collect(unsigned int *pIp, unsigned int numParams);
+	CLEOAPI int CollectNextWithoutIncreasingPC(uint ip);
 
-	CLEOAPI int CollectNextWithoutIncreasingPC(unsigned int ip);
+	CLEOAPI eParamType GetNextParamType() { return ((tParamType*)&game.Scripts.Space[m_dwIp])->type; }
 
-	CLEOAPI eParamType GetNextParamType();
+	CLEOAPI void Store(uint numParams) { game.Scripts.StoreParameters(this, &m_dwIp, numParams); }
 
-	CLEOAPI void Store(unsigned int numParams);
+	CLEOAPI void ReadShortString(char* out);
 
-	CLEOAPI void ReadShortString(char *out);
+	CLEOAPI void UpdateCompareFlag(bool result) { game.Scripts.UpdateCompareFlag(this, result); }
 
-	CLEOAPI void UpdateCompareFlag(bool result);
-
-	CLEOAPI void *GetPointerToScriptVariable();
+	CLEOAPI void* GetPointerToScriptVariable() { return game.Scripts.GetPointerToScriptVariable(this, &m_dwIp, 1); }
 
 	CLEOAPI void JumpTo(int address);
 
 	eOpcodeResult ProcessOneCommand();
 };
 
+static_assert(sizeof(tScriptVar) == 0x04, "tScriptVar size mismatch");
 static_assert(sizeof(CScript) == 0xB0, "CScript size mismatch");
