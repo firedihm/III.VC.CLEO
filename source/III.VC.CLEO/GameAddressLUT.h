c21be2704f3bef1bd205a846aed05dd9fd71819c
firedihm <42544757+firedihm@users.noreply.github.com>
2025-10-27 20:45:39 +0300

    Create GameAddressLUT.h

---
+++ source/III.VC.CLEO/GameAddressLUT.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include "Game.h"
+
+enum eMemoryAddress
+{
+        MA_SCRIPTS_ARRAY,
+        MA_SIZEOF_CRUNNINGSCRIPT,
+        CA_INIT_SCRIPT,
+        CA_PROCESS_ONE_COMMAND,
+        CA_COLLECT_PARAMETERS,
+        CA_COLLECT_NEXT_PARAMETER_WITHOUT_INCREASING_PC,
+        MA_ADD_SCRIPT_TO_LIST,
+        MA_REMOVE_SCRIPT_FROM_LIST,
+        MA_STORE_PARAMETERS,
+        MA_UPDATE_COMPARE_FLAG,
+        MA_GET_POINTER_TO_SCRIPT_VARIABLE,
+        MA_OPCODE_HANDLER_0,
+        MA_OPCODE_HANDLER_1,
+        MA_OPCODE_HANDLER_2,
+        MA_OPCODE_HANDLER_3,
+        MA_OPCODE_HANDLER_4,
+        MA_OPCODE_HANDLER_5,
+        MA_OPCODE_HANDLER_6,
+        MA_OPCODE_HANDLER_7,
+        MA_OPCODE_HANDLER_8,
+        MA_OPCODE_HANDLER_9,
+        MA_OPCODE_HANDLER_10,
+        MA_OPCODE_HANDLER_11,
+        MA_OPCODE_HANDLER_12,
+        MA_OPCODE_HANDLER_13,
+        MA_OPCODE_HANDLER_14,
+        MA_ACTIVE_SCRIPTS_LIST,
+        MA_SCRIPT_PARAMS,
+        MA_SCRIPT_SPACE,
+        MA_NUM_OPCODES_EXECUTED,
+        MA_USED_OBJECT_ARRAY,
+
+        MA_GET_TEXT,
+        CA_GET_TEXT,
+        MA_CTEXT,
+        MA_TEXT_DRAWERS,
+        MA_CURRENT_TEXT_DRAWER,
+        MA_CHEAT_STRING,
+        MA_TEXT_BOX,
+        MA_STYLED_TEXT,
+        MA_TEXT_LOW_PRIORITY,
+        MA_TEXT_HIGH_PRIORITY,
+
+        MA_SCREEN_WIDTH,
+        MA_SCREEN_HEIGHT,
+
+        MA_ASCII_TO_UNICODE,
+        MA_PRINT_STRING,
+        MA_SET_FONT_STYLE,
+        MA_SET_SCALE,
+        MA_SET_COLOR,
+        MA_SET_LEFT_JUSTIFY_ON,
+        MA_SET_DROP_SHADOW_POSITION,
+        MA_SET_PROP_ON,
+
+        MA_PED_POOL,
+        MA_VEHICLE_POOL,
+        MA_OBJECT_POOL,
+        MA_CPLAYERPED_POOL,
+        MA_PED_POOL_GET_STRUCT,
+        MA_VEHICLE_POOL_GET_STRUCT,
+        MA_OBJECT_POOL_GET_STRUCT,
+        MA_PED_POOL_GET_HANDLE,
+        MA_VEHICLE_POOL_GET_HANDLE,
+        MA_OBJECT_POOL_GET_HANDLE,
+
+        MA_INIT_SCRIPTS,
+        CA_INIT_SCRIPTS_0,
+        CA_INIT_SCRIPTS_1,
+        CA_INIT_SCRIPTS_2,
+        MA_SHUTDOWN_GAME,
+        CA_SHUTDOWN_GAME,
+        MA_GAME_SAVE_SCRIPTS,
+        CA_GAME_SAVE_SCRIPTS,
+        MA_DRAW_IN_MENU,
+        CA_DRAW_IN_MENU,
+
+        MA_VEHICLE_MODEL_INFO,
+        MA_ACTIVE_PAD_STATE,
+        MA_MODEL_FOR_WEAPON,
+        MA_CAMERA_WIDESCREEN,
+        MA_CURRENT_WEATHER,
+        MA_MULTIPLY_3X3,
+        MA_RW3D_TRANSFORM_POINTS,
+        MA_GET_USER_DIRECTORY,
+        MA_SPAWN_CAR,
+        MA_BLEND_ANIMATION,
+        MA_IS_BOAT_MODEL,
+
+        NUM_MA
+};
+
+// as long as Game.cpp is the only block including this header, keeping the lookup table here should be okay
+uintptr_t LookUpTable[NUM_GV][NUM_MA] = {
+
+};
