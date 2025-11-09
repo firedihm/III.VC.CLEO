9b6097a7c08c4c9fdc51cd71ff31ef450a7f7e78
firedihm <42544757+firedihm@users.noreply.github.com>
2025-11-10 00:59:34 +0300

    Update Game.cpp

--- source/III.VC.CLEO/Game.cpp
+++ source/III.VC.CLEO/Game.cpp
@@ -308,7 +308,7 @@ GtaGame::OnShutdownGame()
 float
 ScreenCoord(float a)
 {
-		return a*(((float)(*Screen.Height))/900.f);
+		return a*(((float)(*Screen.pHeight))/900.f);
 }
 
 void
@@ -328,10 +328,10 @@ GtaGame::OnMenuDrawing(float x, float y, wchar_t *text)
 	game.Font.SetLeftJustifyOn();
 	wchar_t line[128];
 	swprintf(line, L"CLEO v%d.%d.%d", CLEO_VERSION_MAIN, CLEO_VERSION_MAJOR, CLEO_VERSION_MINOR);
-	game.Font.PrintString(ScreenCoord(30.0f), (float)*game.Screen.Height - ScreenCoord(34.0f), line);
+	game.Font.PrintString(ScreenCoord(30.0f), (float)*game.Screen.pHeight - ScreenCoord(34.0f), line);
 	scriptMgr.numLoadedCustomScripts ?
 	swprintf(line, L"%d %s, %d %s loaded", scriptMgr.numLoadedCustomScripts, scriptMgr.numLoadedCustomScripts == 1? L"script" : L"scripts",
 		CleoPlugins::numLoadedPlugins, CleoPlugins::numLoadedPlugins == 1? L"plugin" : L"plugins") :
 	swprintf(line, L"%d %s loaded", CleoPlugins::numLoadedPlugins, CleoPlugins::numLoadedPlugins == 1 ? L"plugin" : L"plugins");
-	game.Font.PrintString(ScreenCoord(30.0f), (float)*game.Screen.Height - ScreenCoord(20.0f), line);
+	game.Font.PrintString(ScreenCoord(30.0f), (float)*game.Screen.pHeight - ScreenCoord(20.0f), line);
 }
