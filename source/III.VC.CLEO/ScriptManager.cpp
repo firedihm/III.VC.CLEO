181c4f3e304e0ad24bebf3236f8546b0979c647a
firedihm <42544757+firedihm@users.noreply.github.com>
2025-11-14 14:00:08 +0300

    Update ScriptManager.cpp

--- source/III.VC.CLEO/ScriptManager.cpp
+++ source/III.VC.CLEO/ScriptManager.cpp
@@ -14,8 +14,6 @@ std::set<const fs::directory_iterator*> FileSearchHandles;
 
 ScriptManager scriptMgr;
 
-ScriptManager::ScriptManager() : pCusomScripts(nullptr), numLoadedCustomScripts(0) {}
-
 void
 ScriptManager::LoadScripts()
 {
@@ -30,7 +28,6 @@ ScriptManager::LoadScripts()
 								game.Scripts.pfAddScriptToList(script, game.Scripts.ppActiveScripts);
 								script->AddToCustomList(&pCusomScripts);
 								script->m_bIsActive = true;
-								numLoadedCustomScripts++;
 
 								LOGL(LOG_PRIORITY_MEMORY_ALLOCATION, "Loaded custom script \"%s\"", &script->m_acName);
 						} catch (const char* e) {
@@ -50,9 +47,8 @@ ScriptManager::UnloadScripts()
 		CScript* script = pCusomScripts;
 		while (script) {
 				game.Scripts.pfRemoveScriptFromList(script, game.Scripts.ppActiveScripts);
-				numLoadedCustomScripts--;
 
-				LOGL(LOG_PRIORITY_SCRIPT_LOADING, "Unloaded custom script \"%s\"", &script->m_acName);
+				LOGL(LOG_PRIORITY_SCRIPT_LOADING, "Unloading custom script \"%s\"", &script->m_acName);
 
 				CScript* next = script->m_pNextCustom;
 				delete script;
@@ -62,7 +58,7 @@ ScriptManager::UnloadScripts()
 }
 
 void
-ScriptManager::EnableAllScripts()
+ScriptManager::EnableScripts()
 {
 		CScript* script = pCusomScripts;
 		while (script) {
@@ -73,7 +69,7 @@ ScriptManager::EnableAllScripts()
 }
 
 void
-ScriptManager::DisableAllScripts()
+ScriptManager::DisableScripts()
 {
 		CScript* script = pCusomScripts;
 		while (script) {
@@ -81,6 +77,90 @@ ScriptManager::DisableAllScripts()
 				LOGL(LOG_PRIORITY_SCRIPT_LOADING, "Disabled script \"%s\"", &script->m_acName);
 				script = script->m_pNextCustom;
 		}
+}
+
+void
+ScriptManager::OnGameStart()
+{
+		LOGL(LOG_PRIORITY_GAME_EVENT, "--Game Start--");
+
+		UnloadScripts();
+		CustomText::Unload();
+
+		game.Events.pfInitScripts();
+
+		LoadScripts();
+		CustomText::Load();
+}
+
+void
+ScriptManager::OnGameLoad()
+{
+		LOGL(LOG_PRIORITY_GAME_EVENT, "--Game Load--");
+
+		UnloadScripts();
+		CustomText::Unload();
+
+		game.Events.pfInitScripts();
+
+		LoadScripts();
+		CustomText::Load();
+
+		std::for_each(Misc.openedFiles->begin(), Misc.openedFiles->end(), fclose);
+		Misc.openedFiles->clear();
+		std::for_each(Misc.allocatedMemory->begin(), Misc.allocatedMemory->end(), free);
+		Misc.allocatedMemory->clear();
+		std::for_each(Misc.openedHandles->begin(), Misc.openedHandles->end(), CloseHandle);
+		Misc.openedHandles->clear();
+}
+
+void
+ScriptManager::OnGameReload()
+{
+		LOGL(LOG_PRIORITY_GAME_EVENT, "--Game Reload--");
+
+		UnloadScripts();
+		CustomText::Unload();
+
+		game.Events.pfInitScripts();
+
+		LoadScripts();
+		CustomText::Load();
+
+		std::for_each(Misc.openedFiles->begin(), Misc.openedFiles->end(), fclose);
+		Misc.openedFiles->clear();
+		std::for_each(Misc.allocatedMemory->begin(), Misc.allocatedMemory->end(), free);
+		Misc.allocatedMemory->clear();
+		std::for_each(Misc.openedHandles->begin(), Misc.openedHandles->end(), CloseHandle);
+		Misc.openedHandles->clear();
+}
+
+void
+ScriptManager::OnGameSaveAllScripts(uchar* buf, uint* size)
+{
+		LOGL(LOG_PRIORITY_GAME_EVENT, "--Game Save All Scripts--");
+
+		DisableScripts();
+		Events.pfSaveAllScripts(buf, size);
+		EnableScripts();
+}
+
+void
+ScriptManager::OnGameShutdown()
+{
+		LOGL(LOG_PRIORITY_GAME_EVENT, "--Game Shutdown--");
+
+		game.Events.pfCdStreamRemoveImages();
+
+		scriptMgr.UnloadScripts();
+		CustomText::Unload();
+
+		std::for_each(Misc.openedFiles->begin(),.Misc.openedFiles->end(), fclose);
+		Misc.openedFiles->clear();
+		std::for_each(Misc.allocatedMemory->begin(), Misc.allocatedMemory->end(), free);
+		Misc.allocatedMemory->clear();
+		std::for_each(Misc.openedHandles->begin(), Misc.openedHandles->end(), CloseHandle);
+		Misc.openedHandles->clear();
 }
 
 void
@@ -117,125 +197,4 @@ void
 ScriptManager::DeleteFileSearchHandle(const fs::directory_iterator* handle)
 {
 		FileSearchHandles.erase(handle);
-}
-
-void
-GtaGame::InitScripts_OnGameInit()
-{
-		LOGL(LOG_PRIORITY_GAME_EVENT, "--Game Init--");
-
-		scriptMgr.UnloadScripts();
-		CustomText::Unload();
-
-		Events.pfInitScripts_OnGameInit();
-
-		scriptMgr.LoadScripts();
-		CustomText::Load();
-}
-
-void
-GtaGame::InitScripts_OnGameReinit()
-{
-		LOGL(LOG_PRIORITY_GAME_EVENT, "--Game Re-Init--");
-
-		scriptMgr.UnloadScripts();
-		CustomText::Unload();
-
-		Events.pfInitScripts_OnGameReinit();
-
-		scriptMgr.LoadScripts();
-		CustomText::Load();
-
-		std::for_each(Misc.openedFiles->begin(), Misc.openedFiles->end(), fclose);
-		Misc.openedFiles->clear();
-		std::for_each(Misc.allocatedMemory->begin(), Misc.allocatedMemory->end(), free);
-		Misc.allocatedMemory->clear();
-		std::for_each(Misc.openedHandles->begin(), Misc.openedHandles->end(), CloseHandle);
-		Misc.openedHandles->clear();
-}
-
-void
-GtaGame::InitScripts_OnGameSaveLoad()
-{
-		LOGL(LOG_PRIORITY_GAME_EVENT, "--Game Load Save--");
-
-		scriptMgr.UnloadScripts();
-		CustomText::Unload();
-
-		Events.pfInitScripts_OnGameSaveLoad();
-
-		scriptMgr.LoadScripts();
-		CustomText::Load();
-
-		std::for_each(Misc.openedFiles->begin(), Misc.openedFiles->end(), fclose);
-		Misc.openedFiles->clear();
-		std::for_each(Misc.allocatedMemory->begin(), Misc.allocatedMemory->end(), free);
-		Misc.allocatedMemory->clear();
-		std::for_each(Misc.openedHandles->begin(), Misc.openedHandles->end(), CloseHandle);
-		Misc.openedHandles->clear();
-}
-
-void
-GtaGame::OnGameSaveScripts(int a, int b)
-{
-		LOGL(LOG_PRIORITY_GAME_EVENT, "--Game Save Scripts--");
-
-		scriptMgr.DisableAllScripts();
-
-		Events.pfGameSaveScripts(a, b);
-
-		scriptMgr.EnableAllScripts();
-}
-
-void
-GtaGame::OnShutdownGame()
-{
-		LOGL(LOG_PRIORITY_GAME_EVENT, "--Game Shutdown--");
-
-		Events.pfShutdownGame();
-
-		scriptMgr.UnloadScripts();
-		CustomText::Unload();
-
-		std::for_each(Misc.openedFiles->begin(),.Misc.openedFiles->end(), fclose);
-		Misc.openedFiles->clear();
-		std::for_each(Misc.allocatedMemory->begin(), Misc.allocatedMemory->end(), free);
-		Misc.allocatedMemory->clear();
-		std::for_each(Misc.openedHandles->begin(), Misc.openedHandles->end(), CloseHandle);
-		Misc.openedHandles->clear();
-}
-
-float
-ScreenCoord(float a)
-{
-		return a * (*Screen.pHeight / 900.0f);
-}
-
-void
-GtaGame::OnMenuDrawing(float x, float y, wchar_t* text)
-{
-	game.Events.pfDrawInMenu(x, y, text);
-
-	CRGBA color;
-	wchar_t line[128];
-	if (game.IsGta3())
-			color = { 0xEB, 0xAA, 0x32, 0xFF };
-	else
-			color = { 0xFF, 0x96, 0xE1, 0xFF };
-
-	game.Font.pfSetColor(&color);
-	game.Font.pfSetDropShadowPosition(0);
-	game.Font.pfSetPropOn();
-	game.Font.pfSetFontStyle(0);
-	game.Font.pfSetScale(ScreenCoord(0.45f), ScreenCoord(0.7f));
-	game.Font.pfSetJustifyOn();
-	
-	swprintf(line, L"CLEO v%d.%d.%d", CLEO_VERSION_MAIN, CLEO_VERSION_MAJOR, CLEO_VERSION_MINOR);
-	game.Font.pfPrintString(ScreenCoord(30.0f), (float)*game.Screen.pHeight - ScreenCoord(34.0f), line);
-
-	scriptMgr.numLoadedCustomScripts ?
-	swprintf(line, L"%d %s, %d %s loaded", scriptMgr.numLoadedCustomScripts, scriptMgr.numLoadedCustomScripts == 1? L"script" : L"scripts",
-		CleoPlugins::numLoadedPlugins, CleoPlugins::numLoadedPlugins == 1? L"plugin" : L"plugins") :
-	swprintf(line, L"%d %s loaded", CleoPlugins::numLoadedPlugins, CleoPlugins::numLoadedPlugins == 1 ? L"plugin" : L"plugins");
-	game.Font.pfPrintString(ScreenCoord(30.0f), (float)*game.Screen.pHeight - ScreenCoord(20.0f), line);
 }
