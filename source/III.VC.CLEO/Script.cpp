eedd285313916bea7305d57a6384746de674a916
firedihm <42544757+firedihm@users.noreply.github.com>
2025-11-19 19:26:51 +0300

    Update Script.cpp

--- source/III.VC.CLEO/Script.cpp
+++ source/III.VC.CLEO/Script.cpp
@@ -6,13 +6,11 @@
 #include <cstring>
 #include <ifstream>
 
-CScript::CScript(const char* filepath) : m_pNext(nullptr), m_pPrev(nullptr), m_acName(), m_dwIp(0), m_aGosubAddr(), m_nCurrentGosub(0), m_bIsCustom(true), _pad(0),
-								   m_aLVars(), m_aTimers(), m_bIsActive(false), m_bCondResult(false), m_bIsMission(false), m_bAwake(false), m_dwWakeTime(0), m_wIfOp(0),
-								   m_bNotFlag(false), m_bDeathArrestCheckEnabled(true), m_bWastedOrBusted(false), m_bMissionFlag(false), m_bIsPersistent(false), m_bToBeReloaded(false),
-								   m_nLastPedSearchIndex(0), m_nLastVehicleSearchIndex(0), m_nLastObjectSearchIndex(0), _padd(0),
-								   m_pCodeData(nullptr), m_dwBaseIp(0), m_pCleoCallStack(nullptr), m_pNextCustom(nullptr), m_pPrevCustom(nullptr), m_pLocalArray(nullptr)
+CScript::CScript(const char* filepath)
 {
-		std::ifstream file(filepath, std::ios_base::in || std::ios_base::binary)
+		Init();
+
+		std::ifstream file(filepath, std::ios_base::in || std::ios_base::binary);
 
 		size_t filesize = file.seekg(0, std::ios::end).tellg();
 		if (!file || !filesize)
@@ -21,21 +19,25 @@ CScript::CScript(const char* filepath) : m_pNext(nullptr), m_pPrev(nullptr), m_a
 		m_pCodeData = new uchar[filesize];
 		m_dwIp = m_dwBaseIp = (uint)m_pCodeData - (uint)game.Scripts.pScriptSpace;
 		file.seekg(0, std::ios::beg).read(m_pCodeData, filesize);
-
-		std::strncpy(m_acName, &std::strrchr(filepath, '\\')[1], KEY_LENGTH_IN_SCRIPT - 1); // keep '\0' from initializer
-		m_pLocalArray = new tScriptVar[0xFF];
 }
 
 CScript::~CScript()
 {
-		StackFrame* frame = m_pCleoCallStack;
-		while (frame) {
-				StackFrame* prev = frame->prev;
-				delete frame;
-				frame = prev;
-		}
+		while (m_pCleoCallStack)
+				PopStackFrame();
 
 		delete[] m_pLocalArray;
+}
+
+void
+CScript::Init()
+{
+		std::memset(this, 0, sizeof(CScript));
+		std::strcpy(&m_acName, "noname");
+		m_bDeathArrestCheckEnabled = true;
+
+		m_pLocalArray = new tScriptVar[0xFF];
+		std::memset(m_pLocalArray, 0, sizeof(tScriptVar * 0xFF));
 }
 
 void
@@ -48,6 +50,7 @@ CScript::AddToCustomList(CScript** list)
 
 		if (first)
 				first->m_pPrevCustom = this;
+
 		first = this;
 }
 
@@ -58,8 +61,35 @@ CScript::RemoveFromCustomList(CScript** list)
 				m_pPrevCustom->m_pNextCustom = m_pNextCustom;
 		else
 				*list = m_pNextCustom;
+
 		if (m_pNextCustom)
 				m_pNextCustom->m_pPrevCustom = m_pPrevCustom;
+}
+
+void
+CScript::PushStackFrame()
+{
+		// push_front()
+		StackFrame* frame = new StackFrame();
+		frame->prev = m_pCleoCallStack;
+		m_pCleoCallStack = frame;
+
+		std::memcpy(&frame->vars, &m_aLVars, sizeof(frame->vars));
+
+		frame->retAddr = m_dwIp;
+}
+
+void
+CScript::PopStackFrame()
+{
+		m_dwIp = m_pCleoCallStack->retAddr;
+
+		std::memcpy(&m_aLVars, &m_pCleoCallStack->vars, sizeof(m_aLVars));
+
+		// pop_front()
+		StackFrame* prev = m_pCleoCallStack->prev;
+		delete m_pCleoCallStack;
+		m_pCleoCallStack = prev;
 }
 
 eOpcodeResult
@@ -72,46 +102,20 @@ CScript::ProcessOneCommand()
 		m_dwIp += 2;
 
 		if (Opcodes::functions[op]) { // call opcode registered as custom
-				LOGL(LOG_PRIORITY_OPCODE_ID, "%s custom opcode %04X", m_acName, op);
+				LOGL(LOG_PRIORITY_OPCODE_ID, "%s custom opcode %04X", &m_acName, op);
 				eOpcodeResult result = Opcodes::functions[op](this);
 				*game.Scripts.pNumOpcodesExecuted += 1;
 				return result;
 		} else if (op >= CUSTOM_OPCODE_START_ID) { // if opcode isn't registered as custom, but has custom opcode's ID
-				LOGL(LOG_PRIORITY_ALWAYS, "Error (incorrect opcode): %s, %04X", m_acName, op);
+				LOGL(LOG_PRIORITY_ALWAYS, "Error (incorrect opcode): %s, %04X", &m_acName, op);
 				Error("Incorrect opcode ID: %04X", op);
 				return OR_UNDEFINED;
 		} else { // call default opcode
-				LOGL(LOG_PRIORITY_OPCODE_ID, "%s opcode %04X", m_acName, op);
+				LOGL(LOG_PRIORITY_OPCODE_ID, "%s opcode %04X", &m_acName, op);
 				eOpcodeResult result = game.Scripts.OpcodeHandlers[op / 100](this, op);
 				*game.Scripts.pNumOpcodesExecuted += 1;
 				return result;
 		}
-}
-
-void
-CScript::PushStackFrame()
-{
-		// push_front()
-		StackFrame* frame = new StackFrame();
-		frame->prev = m_pCleoCallStack;
-		m_pCleoCallStack = frame;
-
-		std::memcpy(frame->vars, m_aLVars, sizeof(frame->vars));
-
-		frame->retAddr = m_dwIp;
-}
-
-void
-CScript::PopStackFrame()
-{
-		m_dwIp = m_pCleoCallStack->retAddr;
-
-		std::memcpy(m_aLVars, m_pCleoCallStack->vars, sizeof(m_aLVars));
-
-		// pop_front()
-		StackFrame* prev = m_pCleoCallStack->prev;
-		delete m_pCleoCallStack;
-		m_pCleoCallStack = prev;
 }
 
 eParamType
