b088fbf88669b33d756608edf894f004e4b618bf
firedihm <42544757+firedihm@users.noreply.github.com>
2025-11-23 17:45:34 +0300

    Update domain.h

--- source/III.VC.CLEO/domain.h
+++ source/III.VC.CLEO/domain.h
@@ -1,9 +1,9 @@
 #pragma once
 
-// Script parameters have their data types compiled as a byte prefix...
-struct tParamType
+// Script parameters are compiled with byte prefix that denotes their data type...
+struct ScriptParamType
 {
-        enum eParamType : char {
+        enum : char {
                 PARAM_TYPE_END_OF_PARAMS = 0,
                 PARAM_TYPE_INT32 = 1,
                 PARAM_TYPE_GVAR = 2,
@@ -16,8 +16,8 @@ struct tParamType
         bool processed : 1; // strings are compiled as (size byte + char arr); we convert them to c-string at runtime
 };
 
-// ...but ScriptParams array, the virtual call stack, is type-agnostic; so we'll treat it as union
-union tScriptVar
+// ...but ScriptParams array, the virtual call stack, is type-agnostic; so we'll treat it's elements as union
+union ScriptParam
 {
         int nVar;
         float fVar;
@@ -39,4 +39,78 @@ enum eOpcodeResult : char
         OR_CONTINUE = 0,
         OR_TERMINATE = 1,
         OR_UNDEFINED = -1
+};
+
+// for models and objects loaded from scripts
+struct tUsedObject
+{
+        char name[24];
+        int index;
+};
+
+struct CVector
+{
+        float x, y, z;
+};
+
+struct CRect
+{
+        float left, bottom, right, top;
+};
+
+struct CRGBA
+{
+        uchar r, g, b, a;
+};
+
+// stores settings for script-based rectangle drawing, e.g. 038E: DRAW_RECT
+struct intro_script_rectangle 
+{
+	bool m_bIsUsed;
+	bool m_bBeforeFade;
+	short m_nTextureId;
+	CRect m_sRect;
+	CRGBA m_sColor;
+};
+
+// stores settings for script-based string drawing, e.g. 033E: DISPLAY_TEXT
+struct intro_text_line
+{
+        float m_fScaleX;
+        float m_fScaleY;
+        CRGBA m_sColor;
+        bool m_bJustify;
+        bool m_bCentered;
+        bool m_bBackground;
+        bool m_bBackgroundOnly;
+        float m_fWrapX;
+        float m_fCenterSize;
+        CRGBA m_sBackgroundColor;
+        bool m_bTextProportional;
+        bool m_bTextBeforeFade;
+        bool m_bRightJustify;
+        int32 m_nFont;
+        float m_fAtX;
+        float m_fAtY;
+        wchar_t text[500]; // 100 in VC, 500 in III
+};
+
+struct CPool
+{
+        void* m_entries;
+        uchar* m_flags;
+        int m_size;
+        int m_allocPtr;
+};
+
+struct bVehicleFlags
+{
+        uint8_t bIsLawEnforcer : 1; // Is this guy chasing the player at the moment
+        uint8_t bIsAmbulanceOnDuty : 1; // Ambulance trying to get to an accident
+        uint8_t bIsFireTruckOnDuty : 1; // Firetruck trying to get to a fire
+        uint8_t bIsLocked : 1; // Is this guy locked by the script (cannot be removed)
+        uint8_t bEngineOn : 1; // For sound purposes. Parked cars have their engines switched off (so do destroyed cars)
+        uint8_t bIsHandbrakeOn : 1; // How's the handbrake doing ?
+        uint8_t bLightsOn : 1; // Are the lights switched on ?
+        uint8_t bFreebies : 1; // Any freebies left in this vehicle ?
 };
