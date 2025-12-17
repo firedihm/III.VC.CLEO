7f7acf1d3f961979d62366565033c2022734bf1f
firedihm <42544757+firedihm@users.noreply.github.com>
2025-12-17 19:49:12 +0300

    Update Memory.cpp

--- source/III.VC.CLEO/Memory.cpp
+++ source/III.VC.CLEO/Memory.cpp
@@ -2,11 +2,13 @@
 
 #include <Windows.h>
 
+#include <cassert>
 #include <cstring>
 
 void
 memory::Write(void* dest, const void* src, size_t count, bool vp)
 {
+        // practically, if nullptr is passed, then call is skipped
         if (dest < 0x401000)
                 return;
 
@@ -25,21 +27,11 @@ memory::Write(void* dest, const void* src, size_t count, bool vp)
 }
 
 void
-memory::RedirectCall(uchar* dest, uchar* addr)
+memory::Intercept(uchar op, uchar* dest, uchar* addr)
 {
-        uchar op = 0xE8;
+        assert(op == 0xE8 || op == 0xE9);
+
         Write(dest, &op, sizeof(op), true);
-
-        ptrdiff_t offset = addr - (dest + 5);
-        Write(dest + 1, &offset, sizeof(offset), true);
-}
-
-void
-memory::RedirectJump(uchar* dest, uchar* addr)
-{
-        uchar op = 0xE9;
-        Write(dest, &op, sizeof(op), true);
-
         ptrdiff_t offset = addr - (dest + 5);
         Write(dest + 1, &offset, sizeof(offset), true);
 }
