6a111a22870ad7c0378494d4631d81aaed6e646a
firedihm <42544757+firedihm@users.noreply.github.com>
2026-02-18 22:20:21 +0300

    Update Script.h

--- source/III.VC.CLEO/Script.h
+++ source/III.VC.CLEO/Script.h
@@ -2,21 +2,19 @@
 
 #include "domain.h"
 
-enum {
-		KEY_LENGTH_IN_SCRIPT = 8,
-		MAX_STACK_DEPTH = 6,
-		NUM_LOCAL_VARS = 16,
-		NUM_TIMERS = 2,
-		CLEO_ARRAY_SIZE = 256
-};
-
 /*
 	We "inherit" CRunningScript from base game, and add custom data in CCustomScript. 
-	The order of class' members is same for both III and VC, except for 3 flags defined in unions.
+	III and VC have equal number and types of class' members. Order of members differs 
+	only for 3 flags; they are defined in unions below.
 */
 class CRunningScript
 {
 protected:
+		static constexpr int KEY_LENGTH_IN_SCRIPT = 8;
+		static constexpr int MAX_STACK_DEPTH = 6;
+		static constexpr int NUM_LOCAL_VARS = 16;
+		static constexpr int NUM_TIMERS = 2;
+
 		CRunningScript* m_pNext;
 		CRunningScript* m_pPrev;
 		char m_acName[KEY_LENGTH_IN_SCRIPT];
@@ -38,18 +36,21 @@ protected:
 		CRunningScript();
 };
 
+// make sure CRunningScript's composition wasn't changed; as long as member order is correct...
 static_assert(sizeof(CRunningScript) == 0x88);
 
 class CCustomScript : protected CRunningScript
 {
 protected:
+		static constexpr int CLEO_ARRAY_SIZE = 256;
+
 		uchar* m_pCodeData;
 		bool m_bIsCustom;
 		bool m_bIsPersistent;
 		uint m_nLastPedSearchIndex;
 		uint m_nLastVehicleSearchIndex;
 		uint m_nLastObjectSearchIndex;
-		ScriptParam* m_pCleoArray;
+		ScriptParam* CLEO_array_;
 
 		CCustomScript();
 		~CCustomScript();
@@ -58,28 +59,25 @@ protected:
 		void PopStackFrame();
 
 		/*
-			ObjectReferences keep track of script's ownership of objects it 
-			creates (allocated memory, opened files...) to avoid memory leaks: 
-			in case of programmer's fault or premature termination.
+			We register objects script creates (allocated memory, opened files...) and their dtors 
+			to avoid memory leaks: in case of programmer's fault or premature termination.
 		*/
 		template <typename T>
-		void AddReference(T* obj) {
-				m_pObjectReferences = new ObjectReference{m_pObjectReferences, obj, [](void* self) { static_cast<T*>(self)->~T(); }};
-		}
-		void DeleteObject(void* obj);
+		void RegisterObject(T* obj) { register_ = new RegData{register_, obj, [](void* self) { static_cast<T*>(self)->~T(); }}; }
+		void DeleteRegisteredObject(void* obj);
 
 private:
 		struct StackFrame {
 				StackFrame* next;
 				uint ret_addr;
 				ScriptParam vars[NUM_LOCAL_VARS];
-		}* m_pCleoCallStack;
+		}* call_stack_;
 
-		struct ObjectReference {
-				ObjectReference* next;
+		struct RegData {
+				RegData* next;
 				void* obj;
 				void (__thiscall* destruct)(void* self); // non-capturing, dtor-invoking lambda
-		}* m_pObjectReferences;
+		}* register_;
 };
 
 // this is introduced to provide laconic, forward-declarable type alias for CCustomScript.
