aa3ba1957e2d940d46234d99e748453a76058113
firedihm <42544757+firedihm@users.noreply.github.com>
2026-02-19 12:14:29 +0300

    Update Opcodes.h

--- source/III.VC.CLEO/Opcodes.h
+++ source/III.VC.CLEO/Opcodes.h
@@ -6,14 +6,13 @@ class Script;
 
 namespace opcodes
 {
+        using Definition = eOpcodeResult (__stdcall*)(Script*);
+
         inline constexpr ushort CUSTOM_START_ID = 0x05DC;
         inline constexpr ushort MAX_ID = 0x8000;
-
-        using Definition = eOpcodeResult (__stdcall*)(Script*);
 
         inline Definition Definitions[MAX_ID] = {nullptr};
 
         __declspec(dllexport) bool Register(ushort id, Definition def);
-
         void Register(); // initializes standard CLEO opcodes
 }
