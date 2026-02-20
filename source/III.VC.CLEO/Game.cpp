87b7b8942e7d48cbbbbc76faa23dba5a86028ece
firedihm <42544757+firedihm@users.noreply.github.com>
2026-02-20 19:39:15 +0300

    Update Game.cpp

--- source/III.VC.CLEO/Game.cpp
+++ source/III.VC.CLEO/Game.cpp
@@ -92,29 +92,6 @@ namespace hooks {
 		}
 }
 
-eGameVersion
-DetermineGameVersion()
-{
-		uint scan = *(uint*)0x61C11C;
-		if (scan == 0x74FF5064)
-				return GAME_GTAVC_V1_0;
-		if (scan == 0x00408DC0)
-				return GAME_GTAVC_V1_1;
-		if (scan == 0x00004824)
-				return GAME_GTAVC_VSTEAM;
-		if (scan == 0x24E58287)
-				return GAME_GTAVC_VSTEAMENC;
-		if (scan == 0x00598B80)
-				return GAME_GTA3_V1_0;
-		if (scan == 0x00598E40)
-				return GAME_GTA3_V1_1;
-		if (scan == 0x646E6957)
-				return GAME_GTA3_VSTEAM;
-		if (scan == 0x00FFFFFF)
-				return GAME_GTA3_VSTEAMENC;
-		throw "Unsupported game version";
-}
-
 bool
 DetermineChineseness()
 {
@@ -135,6 +112,35 @@ DetermineChineseness()
 		return handle;
 }
 
+const game::Version = []() -> game::Release {
+		if (uint scan = *(uint*)0x61C11C; scan == 0x74FF5064) {
+				return game::Release::VC_1_0;
+		} else if (scan == 0x00408DC0) {
+				return game::Release::VC_1_1;
+		} else if (scan == 0x00004824) {
+				return game::Release::VC_Steam;
+		} else if (scan == 0x00598B80) {
+				return game::Release::III_1_0;
+		} else if (scan == 0x00598E40) {
+				return game::Release::III_1_1;
+		} else if (scan == 0x646E6957) {
+				return game::Release::III_Steam;
+		} else {
+				// compressed steam executables; wait for them to decompress
+				while (scan == 0x24E58287 || scan == 0x00FFFFFF) {
+						std::this_thread::yield();
+
+						if (scan = *(uint*)0x61C11C; scan == 0x00004824) {
+								return game::Release::VC_Steam;
+						else if (scan == 0x646E6957) {
+								return game::Release::III_Steam;
+						}
+				}
+		}
+
+		throw "Unsupported game version";
+}();
+
 GtaGame::GtaGame() : Version(DetermineGameVersion()), is_chinese_(DetermineChineseness()),
 					 kMainSize(IsGta3() ? 128*1024 : 225512), kMissionSize(IsGta3() ? 32*1024 : 35000), kScriptSpaceSize(kMainSize + kMissionSize)
 {
@@ -147,105 +153,95 @@ GtaGame::GtaGame() : Version(DetermineGameVersion()), is_chinese_(DetermineChine
 		GameAddressLUT lut(Version);
 
 		ScriptsArray = new Script[MAX_NUM_SCRIPTS];
-		memory::Write<void*>(lut[MA_SCRIPTS_ARRAY_0], ScriptsArray);
-		memory::Write<void*>(lut[MA_SCRIPTS_ARRAY_1], &ScriptsArray->m_pNext);
-		memory::Write<void*>(lut[MA_SCRIPTS_ARRAY_2], &ScriptsArray->m_pPrev);
-		memory::Write<size_t>(lut[MA_SIZEOF_CRUNNINGSCRIPT_0], sizeof(Script));
-		memory::Write<size_t>(lut[MA_SIZEOF_CRUNNINGSCRIPT_1], sizeof(Script));
-		memory::Intercept<Jump>(lut[CA_INIT_SCRIPT], Script::Init);
-		memory::Intercept<Jump>(lut[CA_PROCESS_ONE_COMMAND], Script::ProcessOneCommand);
-		memory::Intercept<Jump>(lut[CA_COLLECT_PARAMETERS], Script::CollectParameters);
-		memory::Intercept<Jump>(lut[CA_COLLECT_NEXT_PARAMETER_WITHOUT_INCREASING_PC], Script::CollectNextParameterWithoutIncreasingPC);
-		Scripts.pfAddScriptToList = (void (__thiscall*)(Script*, Script**))lut[MA_ADD_SCRIPT_TO_LIST];
-		Scripts.pfRemoveScriptFromList = (void (__thiscall*)(Script*, Script**))lut[MA_REMOVE_SCRIPT_FROM_LIST];
-		Scripts.pfStoreParameters = (void (__thiscall*)(Script*, uint*, short))lut[MA_STORE_PARAMETERS];
-		Scripts.pfUpdateCompareFlag = (void (__thiscall*)(Script*, bool))lut[MA_UPDATE_COMPARE_FLAG];
-		Scripts.pfGetPointerToScriptVariable = (void* (__thiscall*)(Script*, uint*, short))lut[MA_GET_POINTER_TO_SCRIPT_VARIABLE];
-		Scripts.apfOpcodeHandlers[0] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_0];
-		Scripts.apfOpcodeHandlers[1] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_1];
-		Scripts.apfOpcodeHandlers[2] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_2];
-		Scripts.apfOpcodeHandlers[3] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_3];
-		Scripts.apfOpcodeHandlers[4] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_4];
-		Scripts.apfOpcodeHandlers[5] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_5];
-		Scripts.apfOpcodeHandlers[6] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_6];
-		Scripts.apfOpcodeHandlers[7] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_7];
-		Scripts.apfOpcodeHandlers[8] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_8];
-		Scripts.apfOpcodeHandlers[9] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_9];
-		Scripts.apfOpcodeHandlers[10] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_10];
-		Scripts.apfOpcodeHandlers[11] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_11];
-		Scripts.apfOpcodeHandlers[12] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_12];
-		Scripts.apfOpcodeHandlers[13] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_13];
-		Scripts.apfOpcodeHandlers[14] = (eOpcodeResult (__thiscall*)(Script*, int))lut[MA_OPCODE_HANDLER_14];
-		Scripts.ppActiveScriptsList = (Script**)lut[MA_ACTIVE_SCRIPTS];
-		Scripts.pScriptParams = (ScriptParam*)lut[MA_SCRIPT_PARAMS];
-		Scripts.pScriptSpace = (uchar*)lut[MA_SCRIPT_SPACE];
-		Scripts.pNumOpcodesExecuted = (ushort*)lut[MA_NUM_OPCODES_EXECUTED];
-		Scripts.pUsedObjectArray = (tUsedObject*)lut[MA_USED_OBJECT_ARRAY];
+		memory::Write<void*>(lut[ScriptsArray_0], ScriptsArray);
+		memory::Write<void*>(lut[ScriptsArray_1], &ScriptsArray->m_pNext);
+		memory::Write<void*>(lut[ScriptsArray_2], &ScriptsArray->m_pPrev);
+		memory::Write<size_t>(lut[SizeofScript_0], sizeof(Script));
+		memory::Write<size_t>(lut[SizeofScript_1], sizeof(Script));
+		memory::Intercept<Jump>(lut[InitScript], Script::Init);
+		memory::Intercept<Jump>(lut[ProcessOneCommand], Script::ProcessOneCommand);
+		memory::Intercept<Jump>(lut[CollectParameters], Script::CollectParameters);
+		memory::Intercept<Jump>(lut[CollectNextParameterWithoutIncreasingPC], Script::CollectNextParameterWithoutIncreasingPC);
+		Scripts.pfAddScriptToList = (void (__thiscall*)(Script*, Script**))lut[AddScriptToList];
+		Scripts.pfRemoveScriptFromList = (void (__thiscall*)(Script*, Script**))lut[RemoveScriptFromList];
+		Scripts.pfStoreParameters = (void (__thiscall*)(Script*, uint*, short))lut[StoreParameters];
+		Scripts.pfUpdateCompareFlag = (void (__thiscall*)(Script*, bool))lut[UpdateCompareFlag];
+		Scripts.pfGetPointerToScriptVariable = (void* (__thiscall*)(Script*, uint*, short))lut[GetPointerToScriptVariable];
+		Scripts.apfOpcodeHandlers[0] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_0];
+		Scripts.apfOpcodeHandlers[1] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_1];
+		Scripts.apfOpcodeHandlers[2] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_2];
+		Scripts.apfOpcodeHandlers[3] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_3];
+		Scripts.apfOpcodeHandlers[4] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_4];
+		Scripts.apfOpcodeHandlers[5] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_5];
+		Scripts.apfOpcodeHandlers[6] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_6];
+		Scripts.apfOpcodeHandlers[7] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_7];
+		Scripts.apfOpcodeHandlers[8] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_8];
+		Scripts.apfOpcodeHandlers[9] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_9];
+		Scripts.apfOpcodeHandlers[10] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_10];
+		Scripts.apfOpcodeHandlers[11] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_11];
+		Scripts.apfOpcodeHandlers[12] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_12];
+		Scripts.apfOpcodeHandlers[13] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_13];
+		Scripts.apfOpcodeHandlers[14] = (eOpcodeResult (__thiscall*)(Script*, int))lut[OpcodeHandler_14];
+		Scripts.ppActiveScriptsList = (Script**)lut[ActiveScripts];
+		Scripts.pScriptParams = (ScriptParam*)lut[ScriptParams];
+		Scripts.pScriptSpace = (uchar*)lut[ScriptSpace];
+		Scripts.pNumOpcodesExecuted = (ushort*)lut[CommandsExecuted];
+		Scripts.pUsedObjectArray = (tUsedObject*)lut[UsedObjectArray];
 
-		Text.pfGet = (wchar_t* (__thiscall*)(void*, const char*))lut[MA_GET_TEXT];
-		memory::Write<uint>(lut[MA_VC_ASM_0], 0xD98B5553); // push ebx; push ebp; mov ebx,ecx
-		memory::Write<uint>(lut[MA_VC_ASM_1], 0xE940EC83); // sub esp,40
-		memory::Write<uint>(lut[MA_VC_ASM_2], 0x00000189); // jmp 584F37
-		memory::Intercept<Jump>(lut[CA_GET_TEXT], fxt::Get);
-		Text.pTheText = (void*)lut[MA_THE_TEXT];
-		Text.pIntroTextLines = (intro_text_line*)lut[MA_INTRO_TEXT_LINES];
-		Text.pNumberOfIntroTextLinesThisFrame = (ushort*)lut[MA_NUMBER_OF_INTRO_TEXT_LINES_THIS_FRAME];
-		Text.szKeyboardCheatString = (char*)lut[MA_KEYBOARD_CHEAT_STRING];
-		Text.pfSetHelpMessage = (void (__cdecl*)(wchar_t*, bool, bool))lut[MA_SET_HELP_MESSAGE];
-		Text.pfAddBigMessageQ = (void (__cdecl*)(wchar_t*, uint, ushort))lut[MA_ADD_BIG_MESSAGE_Q];
-		Text.pfAddMessage = (void (__cdecl*)(wchar_t*, uint, ushort))lut[MA_ADD_MESSAGE];
-		Text.pfAddMessageJumpQ = (void (__cdecl*)(wchar_t*, uint, ushort))lut[MA_ADD_MESSAGE_JUMP_Q];
+		Text.pfGet = (wchar_t* (__thiscall*)(void*, const char*))lut[SearchText];
+		memory::Write<uint>(lut[SearchText_asm0], 0xD98B5553); // push ebx; push ebp; mov ebx,ecx
+		memory::Write<uint>(lut[SearchText_asm1], 0xE940EC83); // sub esp,40
+		memory::Write<uint>(lut[SearchText_asm2], 0x00000189); // jmp 584F37
+		memory::Intercept<Jump>(lut[GetText], fxt::Get);
+		Text.pTheText = (void*)lut[TheText];
+		Text.pIntroTextLines = (intro_text_line*)lut[IntroTextLines];
+		Text.pNumberOfIntroTextLinesThisFrame = (ushort*)lut[NumberOfIntroTextLinesThisFrame];
+		Text.szKeyboardCheatString = (char*)lut[KeyboardCheatString];
+		Text.pfSetHelpMessage = (void (__cdecl*)(wchar_t*, bool, bool))lut[SetHelpMessage];
+		Text.pfAddBigMessageQ = (void (__cdecl*)(wchar_t*, uint, ushort))lut[AddBigMessageQ];
+		Text.pfAddMessage = (void (__cdecl*)(wchar_t*, uint, ushort))lut[AddMessage];
+		Text.pfAddMessageJumpQ = (void (__cdecl*)(wchar_t*, uint, ushort))lut[AddMessageJumpQ];
 
-		Font.pfAsciiToUnicode = (void (__cdecl*)(const char*, wchar_t*))lut[MA_ASCII_TO_UNICODE];
-		Font.pfPrintString = (void (__cdecl*)(float, float, wchar_t*))lut[MA_PRINT_STRING];
-		Font.pfSetFontStyle = (void (__cdecl*)(short))lut[MA_SET_FONT_STYLE];
-		Font.pfSetScale = (void (__cdecl*)(float, float))lut[MA_SET_SCALE];
-		Font.pfSetColor = (void (__cdecl*)(CRGBA*))lut[MA_SET_COLOR];
-		Font.pfSetJustifyOn = (void (__cdecl*)())lut[MA_SET_JUSTIFY_ON];
-		Font.pfSetDropShadowPosition = (void (__cdecl*)(short))lut[MA_SET_DROP_SHADOW_POSITION];
-		Font.pfSetPropOn = (void (__cdecl*)())lut[MA_SET_PROP_ON];
+		Pools.ppPedPool = (CPool**)lut[PedPool];
+		Pools.ppVehiclePool = (CPool**)lut[VehiclePool];
+		Pools.ppObjectPool = (CPool**)lut[ObjectPool];
+		Pools.pPlayers = (uchar*)lut[Players];
+		Pools.pfPedPoolGetAt = (void* (__thiscall*)(CPool*, int))lut[PedPoolGetAt];
+		Pools.pfVehiclePoolGetAt = (void* (__thiscall*)(CPool*, int))lut[VehiclePoolGetAt];
+		Pools.pfObjectPoolGetAt = (void* (__thiscall*)(CPool*, int))lut[ObjectPoolGetAt];
+		Pools.pfPedPoolGetHandle = (int (__thiscall*)(CPool*, void*))lut[PedPoolGetIndex];
+		Pools.pfVehiclePoolGetHandle = (int (__thiscall*)(CPool*, void*))lut[VehiclePoolGetIndex];
+		Pools.pfObjectPoolGetHandle = (int (__thiscall*)(CPool*, void*))lut[ObjectPoolGetIndex];
 
-		Pools.ppPedPool = (CPool**)lut[MA_PED_POOL];
-		Pools.ppVehiclePool = (CPool**)lut[MA_VEHICLE_POOL];
-		Pools.ppObjectPool = (CPool**)lut[MA_OBJECT_POOL];
-		Pools.pPlayers = (uchar*)lut[MA_PLAYERS];
-		Pools.pfPedPoolGetAt = (void* (__thiscall*)(CPool*, int))lut[MA_PED_POOL_GET_AT];
-		Pools.pfVehiclePoolGetAt = (void* (__thiscall*)(CPool*, int))lut[MA_VEHICLE_POOL_GET_AT];
-		Pools.pfObjectPoolGetAt = (void* (__thiscall*)(CPool*, int))lut[MA_OBJECT_POOL_GET_AT];
-		Pools.pfPedPoolGetHandle = (int (__thiscall*)(CPool*, void*))lut[MA_PED_POOL_GET_HANDLE];
-		Pools.pfVehiclePoolGetHandle = (int (__thiscall*)(CPool*, void*))lut[MA_VEHICLE_POOL_GET_HANDLE];
-		Pools.pfObjectPoolGetHandle = (int (__thiscall*)(CPool*, void*))lut[MA_OBJECT_POOL_GET_HANDLE];
+		Events.pfInitScripts = (void (__cdecl*)())lut[InitScripts];
+		memory::Intercept<Call>(lut[Init_Scripts_call0], hooks::OnGameStart);
+		memory::Intercept<Call>(lut[Init_Scripts_call1], hooks::OnGameLoad);
+		memory::Intercept<Call>(lut[Init_Scripts_call2], hooks::OnGameReload);
+		Events.pfSaveAllScripts = (void (__cdecl*)(uchar*, uint*))lut[SaveAllScripts];
+		memory::Intercept<Call>(lut[SaveAllScripts_call], hooks::OnGameSaveAllScripts);
+		Events.pfCdStreamRemoveImages = (void (__cdecl*)())lut[CdStreamRemoveImages];
+		memory::Intercept<Call>(lut[CdStreamRemoveImages_call], hooks::OnGameShutdown);
 
-		Events.pfInitScripts = (void (__cdecl*)())lut[MA_INIT_SCRIPTS];
-		memory::Intercept<Call>(lut[CA_INIT_SCRIPTS_ON_GAME_START], hooks::OnGameStart);
-		memory::Intercept<Call>(lut[CA_INIT_SCRIPTS_ON_GAME_LOAD], hooks::OnGameLoad);
-		memory::Intercept<Call>(lut[CA_INIT_SCRIPTS_ON_GAME_RELOAD], hooks::OnGameReload);
-		Events.pfSaveAllScripts = (void (__cdecl*)(uchar*, uint*))lut[MA_SAVE_ALL_SCRIPTS];
-		memory::Intercept<Call>(lut[CA_SAVE_ALL_SCRIPTS], hooks::OnGameSaveAllScripts);
-		Events.pfCdStreamRemoveImages = (void (__cdecl*)())lut[MA_CD_STREAM_REMOVE_IMAGES];
-		memory::Intercept<Call>(lut[CA_CD_STREAM_REMOVE_IMAGES], hooks::OnGameShutdown);
+		Shadows.pfStoreShadowToBeRendered = (float(__cdecl*)(uchar, void*, CVector*, float, float, float, float, short, uchar, uchar, uchar, float, bool, float, void*, bool))lut[StoreShadowToBeRendered];
+		Shadows.ppShadowCarTex = (void**)lut[ShadowCarTex];
+		Shadows.ppShadowPedTex = (void**)lut[ShadowPedTex];
+		Shadows.ppShadowHeliTex = (void**)lut[ShadowHeliTex];
+		Shadows.ppShadowBikeTex = (void**)lut[ShadowBikeTex];
+		Shadows.ppShadowBaronTex = (void**)lut[ShadowBaronTex];
+		Shadows.ppShadowExplosionTex = (void**)lut[ShadowExplosionTex];
+		Shadows.ppShadowHeadLightsTex = (void**)lut[ShadowHeadLightsTex];
+		Shadows.ppBloodPoolTex = (void**)lut[ShadowBloodPoolTex];
 
-		Shadows.pfStoreShadowToBeRendered = (float(__cdecl*)(uchar, void*, CVector*, float, float, float, float, short, uchar, uchar, uchar, float, bool, float, void*, bool))lut[MA_STORE_SHADOW_TO_BE_RENDERED];
-		Shadows.ppShadowCarTex = (void**)lut[MA_SHADOW_CAR_TEX];
-		Shadows.ppShadowPedTex = (void**)lut[MA_SHADOW_PED_TEX];
-		Shadows.ppShadowHeliTex = (void**)lut[MA_SHADOW_HELI_TEX];
-		Shadows.ppShadowBikeTex = (void**)lut[MA_SHADOW_BIKE_TEX];
-		Shadows.ppShadowBaronTex = (void**)lut[MA_SHADOW_RCBARON_TEX];
-		Shadows.ppShadowExplosionTex = (void**)lut[MA_SHADOW_EXPLOSION_TEX];
-		Shadows.ppShadowHeadLightsTex = (void**)lut[MA_SHADOW_HEADLIGHTS_TEX];
-		Shadows.ppBloodPoolTex = (void**)lut[MA_BLOOD_POOL_TEX];
-
-		Misc.pVehicleModelStore = (uchar*)lut[MA_VEHICLE_MODEL_STORE];
-		Misc.pPadNewState = (short*)lut[MA_PAD_NEW_STATE];
-		Misc.pWideScreenOn = (bool*)lut[MA_CAMERA_WIDESCREEN];
-		Misc.pOldWeatherType = (short*)lut[MA_CURRENT_WEATHER];
-		Misc.szRootDirName = (char*)lut[MA_ROOT_DIR_NAME];
-		Misc.pfGetUserFilesFolder = (char* (__cdecl*)())lut[MA_GET_USER_FILES_FOLDER];
-		Misc.pfModelForWeapon = (int (__cdecl*)(int))lut[MA_MODEL_FOR_WEAPON];
-		Misc.pfSpawnCar = (void (__cdecl*)(int))lut[MA_SPAWN_CAR];
-		Misc.pfRwV3dTransformPoints = (void (__cdecl*)(CVector*, const CVector*, int, const void*))lut[MA_RWV3D_TRANSFORM_POINTS];
-		Misc.pfBlendAnimation = (int (__cdecl*)(void*, int, int, float))lut[MA_BLEND_ANIMATION];
-		Misc.pWantToRestart = (bool*)lut[MA_WANT_TO_RESTART];
+		Misc.pVehicleModelStore = (uchar*)lut[VehicleModelStore];
+		Misc.pPadNewState = (short*)lut[PadNewState];
+		Misc.pWideScreenOn = (bool*)lut[WideScreenOn];
+		Misc.pOldWeatherType = (short*)lut[CurrentWeather];
+		Misc.szRootDirName = (char*)lut[RootDirName];
+		Misc.pfGetUserFilesFolder = (char* (__cdecl*)())lut[GetUserFilesFolder];
+		Misc.pfModelForWeapon = (int (__cdecl*)(int))lut[ModelForWeapon];
+		Misc.pfSpawnCar = (void (__cdecl*)(int))lut[SpawnCar];
+		Misc.pfRwV3dTransformPoints = (void (__cdecl*)(CVector*, const CVector*, int, const void*))lut[RwV3dTransformPoints];
+		Misc.pfBlendAnimation = (int (__cdecl*)(void*, int, int, float))lut[BlendAnimation];
 
 		IntroTextLines = new intro_text_line[MAX_NUM_INTRO_TEXT_LINES];
  		IntroRectangles = new intro_script_rectangle[MAX_NUM_INTRO_RECTANGLES];
