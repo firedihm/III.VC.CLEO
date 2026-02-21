a4bb86cc17147c0c50dc4c75f674aac5cdd9f88d
firedihm <42544757+firedihm@users.noreply.github.com>
2026-02-21 21:33:13 +0300

    Update Memory.cpp

--- source/III.VC.CLEO/Memory.cpp
+++ source/III.VC.CLEO/Memory.cpp
@@ -30,7 +30,7 @@ memory::Write(void* dest, void* src, size_t count, bool vp)
 
         uint oldProtect;
         if (vp)
-                VirtualProtect(dest, count, PAGE_EXECUTE_READWRITE, &oldProtect);
+                ::VirtualProtect(dest, count, PAGE_EXECUTE_READWRITE, &oldProtect);
 
         switch (count) {
         case 1:
@@ -48,7 +48,7 @@ memory::Write(void* dest, void* src, size_t count, bool vp)
         }
 
         if (vp)
-                VirtualProtect(dest, count, oldProtect, &oldProtect);
+                ::VirtualProtect(dest, count, oldProtect, &oldProtect);
 }
 
 void
@@ -65,11 +65,11 @@ memory::Intercept(uchar op, void* dest, void* addr)
 void*
 memory::LoadLibrary(const char* name)
 {
-		return LoadLibraryA(name);
+		return ::LoadLibraryA(name);
 }
 
 void
 memory::FreeLibrary(const void* handle)
 {
-		FreeLibrary(handle);
+		::FreeLibrary(handle);
 }
