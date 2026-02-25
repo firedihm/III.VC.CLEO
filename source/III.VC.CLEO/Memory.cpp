3ef8d63d9372a5ee7e034c1203d94431d0749097
firedihm <42544757+firedihm@users.noreply.github.com>
2026-02-25 18:40:41 +0300

    Update Memory.cpp

--- source/III.VC.CLEO/Memory.cpp
+++ source/III.VC.CLEO/Memory.cpp
@@ -7,9 +7,6 @@
 int
 memory::Read(void* dest, size_t count, bool vp)
 {
-        if (!dest)
-                return;
-
 		switch (count) {
 		case 1:
 				return *(char*)dest;
@@ -25,9 +22,6 @@ memory::Read(void* dest, size_t count, bool vp)
 void
 memory::Write(void* dest, void* src, size_t count, bool vp)
 {
-        if (!dest)
-                return;
-
         uint oldProtect;
         if (vp)
                 ::VirtualProtect(dest, count, PAGE_EXECUTE_READWRITE, &oldProtect);
@@ -54,12 +48,10 @@ memory::Write(void* dest, void* src, size_t count, bool vp)
 void
 memory::Intercept(uchar op, void* dest, void* addr)
 {
-        if (!dest)
-                return;
+		ptrdiff_t offset = (uchar*)addr - ((uchar*)dest + 5); // (uchar*) cast allows us to perform bytewise arithmetic on ptrs
 
-        Write(dest, &op, sizeof(op), true);
-        ptrdiff_t offset = (uchar*)addr - ((uchar*)dest + 5); // (uchar*) cast allows us to perform bytewise arithmetic on ptrs
-        Write((uchar*)dest + 1, &offset, sizeof(offset), true);
+		Write(dest, op);
+		Write((uchar*)dest + 1, offset);
 }
 
 void*
