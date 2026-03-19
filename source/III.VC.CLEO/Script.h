7c711aa0375146ee1a4486f455afc32710690ef5
firedihm <42544757+firedihm@users.noreply.github.com>
2026-03-19 18:05:54 +0300

    Update Script.h

--- source/III.VC.CLEO/Script.h
+++ source/III.VC.CLEO/Script.h
@@ -79,7 +79,7 @@ public:
 		__declspec(dllexport) void StoreParameters(short num_params);
 		__declspec(dllexport) void UpdateCompareFlag(bool result);
 
-		__declspec(dllexport) void* GetPointerToScriptVariable();
+		__declspec(dllexport) ScriptParam* GetPointerToScriptVariable();
 		__declspec(dllexport) void Jump(int address);
 		__declspec(dllexport) void FormatString(char* out, const char* format);
 
