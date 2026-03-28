e783b9e83fa942b3f6d182560ddce0006ab2a6b8
firedihm <42544757+firedihm@users.noreply.github.com>
2026-03-28 10:25:41 +0300

    Update Fxt.h

--- source/III.VC.CLEO/Fxt.h
+++ source/III.VC.CLEO/Fxt.h
@@ -2,12 +2,12 @@
 
 namespace fxt
 {
-		void Add(const char* key, const char* text);
-		void Remove(const char* key);
+		void add(const char* key, const char* text);
+		void remove(const char* key);
 
-		void LoadEntries();
-		void UnloadEntries();
+		void load_entries();
+		void unload_entries();
 
 		// this is never called; instead, we jump here from CText::Get(const char* key), which is a __thiscall 
-		wchar_t* __thiscall Get(void* pTheText, const char* key);
+		wchar_t* __thiscall get(void* pTheText, const char* key);
 };
