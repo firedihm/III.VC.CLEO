34538b42fb8f8a62dddc5af6f15fdff80ec4f9ca
firedihm <42544757+firedihm@users.noreply.github.com>
2026-03-28 12:21:54 +0300

    Update Fxt.cpp

--- source/III.VC.CLEO/Fxt.cpp
+++ source/III.VC.CLEO/Fxt.cpp
@@ -1,6 +1,7 @@
 #include "Fxt.h"
 #include "Game.h"
 
+#include <cctype>
 #include <codecvt>
 #include <cstring>
 #include <filesystem>
@@ -13,29 +14,39 @@ namespace fs = std::filesystem;
 
 std::map<std::string, std::wstring> g_fxt_entries;
 
+std::string
+to_upper(const char* c_str)
+{
+		std::string result(c_str);
+		for (char& c : result)
+				c = std::toupper(c);
+
+		return result;
+}
+
 void
-fxt::Add(const char* key, const char* text)
+fxt::add(const char* key, const char* text)
 {
 		// CJK support makes game text Unicode; it's Extended ASCII with custom encoding by default
 		std::wstring wide_text;
-		if (game::CjkSupportLib) {
+		if (game::cjk_lib) {
 				std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 				wide_text = converter.from_bytes(text);
 		} else {
 				wide_text = std::wstring(text, text + std::strlen(text));
 		}
 
-		g_fxt_entries.emplace(key, std::move(wide_text))
+		g_fxt_entries.emplace(to_upper(key), std::move(wide_text))
 }
 
 void
-fxt::Remove(const char* key)
+fxt::remove(const char* key)
 {
-		g_fxt_entries.erase(key);
+		g_fxt_entries.erase(to_upper(key));
 }
 
 void
-fxt::LoadEntries()
+fxt::load_entries()
 {
 		fs::path dir = fs::path(game::RootDirName) / "CLEO" / "CLEO_TEXT";
 
@@ -61,23 +72,25 @@ fxt::LoadEntries()
 								if (line[text_start] == '\\' && line[text_start + 1] == '$')
 										text_start += 2;
 
-								Add(line.substr(key_start, key_end).c_str(), line.substr(text_start).c_str());
+								add(line.substr(key_start, key_end).c_str(), line.substr(text_start).c_str());
 						}
 				}
 		}
 }
 
 void
-fxt::UnloadEntries()
+fxt::unload_entries()
 {
 		g_fxt_entries.clear();
 }
 
 wchar_t*
-fxt::Get(void* pTheText, const char* key)
+fxt::get(void* pTheText, const char* key)
 {
-		if (auto it = g_fxt_entries.find(key); it != g_fxt_entries.end())
+		std::string key_upper = to_upper(key);
+
+		if (auto it = g_fxt_entries.find(key_upper); it != g_fxt_entries.end())
 				return it->second.c_str();
 		else
-				return game::GetText(pTheText, key);
+				return game::GetText(pTheText, key_upper.c_str());
 }
