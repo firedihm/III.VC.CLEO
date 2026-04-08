9d470a1af2e3b65d5c815b11bed239747b930ef0
firedihm <42544757+firedihm@users.noreply.github.com>
2026-04-08 18:13:58 +0300

    Update DllMain.cpp

--- source/III.VC.CLEO/DllMain.cpp
+++ source/III.VC.CLEO/DllMain.cpp
@@ -11,8 +11,8 @@ APIENTRY DllMain(HMODULE hModule, DWORD reason_for_call, LPVOID reserved)
 {
         switch (reason_for_call) {
         case DLL_PROCESS_ATTACH:
-                game::expand_memory();
                 log::open();
+                game::expand_memory();
                 plugins::load();
                 script_mgr::load_scripts(true);
                 break;
@@ -22,10 +22,16 @@ APIENTRY DllMain(HMODULE hModule, DWORD reason_for_call, LPVOID reserved)
         case DLL_PROCESS_DETACH:
                 script_mgr::unload_scripts(true);
                 plugins::unload();
-                log::close();
                 game::free_memory();
+                log::close();
                 break;
         }
 
         return TRUE;
+}
+
+namespace cleo {
+        __declspec(dllexport) uint version() { return 2 << 24 | 2 << 16 | 0 << 8 ; }
+        __declspec(dllexport) uchar* script_space() { return game::ScriptSpace; }
+        __declspec(dllexport) ScriptParam* script_params() { return game::ScriptParams; }
 }
